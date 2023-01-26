a2a0881c889384d4140e185eab8c7d9ca65e6413
id-b3 <58172188+id-b3@users.noreply.github.com>
2023-01-26 15:26:24 +0100

    Data merging makefile

--- Makefile
+++ Makefile
@@ -22,17 +22,19 @@ endif
 
 ## Install Python Dependencies
 requirements: test_environment
-	$(PYTHON_INTERPRETER) -m pip install -U pip setuptools wheel
 	$(PYTHON_INTERPRETER) -m pip install -r requirements.txt
 
 ## Make Dataset
-data: requirements
-	$(PYTHON_INTERPRETER) src/data/make_dataset.py data/raw data/processed
+data:
+	$(MAKE) -f ./src/data/make_data.mk -C ./
+	# $(PYTHON_INTERPRETER) src/data/make_dataset.py data/raw data/processed
 
 ## Delete all compiled Python files
 clean:
 	find . -type f -name "*.py[co]" -delete
 	find . -type d -name "__pycache__" -delete
+	rm -rf ./data/interim/*
+	rm -rfi ./data/processed/*
 
 ## Lint using flake8
 lint:
---
+++ src/data/make_data.mk
@@ -0,0 +1,32 @@
+SHELL := bash
+.ONESHELL:
+.SHELLFLAGS := -eu -o pipefail -c
+
+.PHONY: all clean
+
+VPATH=./data/raw/
+DINT=./data/interim/
+DPRC=./data/processed/
+
+SPSS_FILES=1a_q_1.sav 1a_v_1.sav 2a_q_2.sav 2a_v_1.sav
+CSV_FILES=$(DINT)1a_q_1.csv $(DINT)1a_v_1.csv $(DINT)2a_q_2.csv $(DINT)2a_v_1.csv
+
+SEG_CSV=$(VPATH)segmentation_review_final.csv
+MRG_CSV=$(DINT)data_merged.csv
+BP_CSV=$(DPRC)bronchial_parameter_db.csv
+
+
+all: $(BP_CSV)
+	echo "Done"
+
+# Remove any columns that contain only 1 unique value
+$(BP_CSV): $(MRG_CSV)
+	< $< csvcut -C $$(csvstat $< --unique | grep ': 1$$' | cut -d. -f 1 | tr -d ' ' | paste -sd,) > $@
+
+# Merge all CSV files into one using the patientID as an index
+$(MRG_CSV): $(CSV_FILES) $(SEG_CSV)
+	csvjoin -c patientID $(CSV_FILES) <(sed 's/participant_id/patientID/' $(SEG_CSV)) > $@
+
+# Convert the SPSS files into CSV files
+$(CSV_FILES): $(DINT)%.csv : %.sav
+	pspp-convert $< $@
