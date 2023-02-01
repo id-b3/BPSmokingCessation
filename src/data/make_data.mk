792766b67b9552f47c0d7d4cc1069ff0068b6169
id-b3 <58172188+id-b3@users.noreply.github.com>
2023-02-01 11:28:02 +0100

    Reformat output, add filter as final step

--- src/data/make_data.mk
+++ src/data/make_data.mk
@@ -17,15 +17,18 @@ SEG_CSV=segmentation_review_final.csv
 SCN_CSV=participant_age-at-scan.csv
 SPLT_CSV=$(DINT)formatted_bp_data.csv
 MRG_CSV=$(DINT)data_merged.csv
-BP_CSV=$(DPRC)bronchial_parameter_db_full.csv
-BP_FILT_CSV=$(DPRC)bp_db_filtered.csv
-
+BP_CSV=$(DINT)bronchial_parameter_db_full.csv
+BP_FILT_CSV=$(DINT)bp_db_filtered.csv
+BP_FINAL=$(DPRC)final_bp_db.csv
 
 DB_COLS=1,2,311,308,309,88,89,90,185,186,187,36,167,37,168,24,147,48,169,62,170,67,175,74,75,178,179,15,16,152,226,4,117,222,149,10,12,13,19,85,164,104,109,113,206,211,216,110,212,123,124,231,232,310,255,258,259,260,251,252,253,254,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,300,301,302,303,304,305,306
 
 
-all: $(BP_FILT_CSV)
+all: $(BP_FINAL)
 	echo "Done"
+
+$(BP_FINAL): $(BP_FILT_CSV)
+	< $< ./src/data/fill_missing.py
 
 $(BP_FILT_CSV): $(BP_CSV)
 	< $< csvcut -c $(DB_COLS) > $@
