b5919dbfd383e8aa1fce9f9f21c61caeb68ce4fb
id-b3 <58172188+id-b3@users.noreply.github.com>
2023-02-01 11:27:28 +0100

    Filter spirometry with LLN, add age categories

--- src/data/fill_missing.py
+++ src/data/fill_missing.py
@@ -1,12 +1,19 @@
+#!/usr/bin/env python
+
 import sys
+from pathlib import Path
+import numpy as np
 import pandas as pd
 
 df = pd.read_csv(sys.stdin, index_col=0)
+outpath = Path("./data/processed/")
+
 
 # ------ PARTICIPANT CHARACTERISTICS
 # Fill in missing gender values, drop the other columns
 df['gender'].fillna(df['gender_first'], inplace=True)
-df['age_at_scan'].replace('#NUM!', 0, inplace=True)
+df['age_at_scan'].replace('#NUM!', np.nan, inplace=True)
+df['age_at_scan'] = df['age_at_scan'].astype(float)
 df['weight_at_scan'].fillna(df['bodyweight_kg_all_m_1_max2'], inplace=True)
 df['length_at_scan'].fillna(df['bodylength_cm_all_m_1_max2'], inplace=True)
 df['length_at_scan'].fillna(df['bodylength_cm_all_m_1_max'], inplace=True)
@@ -18,6 +25,28 @@ df.drop([
 ],
         axis=1,
         inplace=True)
+
+# Create age categories
+age_label_5 = [
+    '40-45', '45-50', '50-55', '55-60', '60-65', '65-70', '70-75', '75-80',
+    '80-85', '85-90', '90-95', '95-100'
+]
+age_cut_5 = np.linspace(40, 100, 13)
+
+df['age_5yr'] = pd.cut(df['age_at_scan'],
+                       bins=age_cut_5,
+                       labels=age_label_5,
+                       right=False)
+
+age_label_10 = [
+    '40-50', '50-60', '60-70', '70-80', '80-90', '90-100'
+]
+age_cut_10 = np.linspace(40, 100, 7)
+
+df['age_10yr'] = pd.cut(df['age_at_scan'],
+                        bins=age_cut_10,
+                        labels=age_label_10,
+                        right=False)
 
 # ------ SMOKING
 # Smoking merge and fill
@@ -85,3 +114,50 @@ df.drop([
 ],
         inplace=True,
         axis=1)
+
+# ------ SPIROMETRY
+df['fev1'] = df['spirometry_fev1_all_m_1_max2'].fillna(
+    df['spirometry_fev1_all_m_1_max'])
+df['fvc'] = df['spirometry_fvc_all_m_1_max2'].fillna(
+    df['spirometry_fvc_all_m_1_max'])
+df['fev1_pp'] = df['fev1_percpredicted_all_c_1_max2'].fillna(
+    df['fev1_percpredicted_all_c_1_max'])
+df['fev1fvc_lln'] = df['fev1fvc_lowerlimit_all_c_1_max2'].fillna(
+    df['fev1fvc_lowerlimit_all_c_1_max'])
+df['fev1_fvc'] = df.fev1 / df.fvc
+
+df.drop([
+    'spirometry_fev1_all_m_1_max2', 'spirometry_fev1_all_m_1_max',
+    'spirometry_fvc_all_m_1_max', 'spirometry_fvc_all_m_1_max2',
+    'fev1_percpredicted_all_c_1_max2', 'fev1_percpredicted_all_c_1_max',
+    'fev1fvc_lowerlimit_all_c_1_max2', 'fev1fvc_lowerlimit_all_c_1_max',
+    'fev1_lowerlimit_all_c_1_max2', 'fev1_lowerlimit_all_c_1_max',
+    'fvc_lowerlimit_all_c_1_max2', 'fvc_lowerlimit_all_c_1_max'
+],
+        inplace=True,
+        axis=1)
+
+# ------ COPD GOLD Staging
+# GOLD 0: FEV1/FVC > fev1fvc_lln
+# GOLD 1: FEV1/FVC < fev1fvc_lln & fev1_pp > 80
+# GOLD 2: FEV1/FVC < fev1fvc_lln & fev1_pp 50-80
+# GOLD 3: FEV1/FVC < fev1fvc_lln & fev1_pp 50-30
+# GOLD 4: FEV1/FVC < fev1fvc_lln & fev1_pp < 30
+
+criteria = [
+    ((df.fev1_pp >= 80) & (df.fev1_fvc < df.fev1fvc_lln)),
+    ((df.fev1_pp < 80) & (df.fev1_pp >= 50) & (df.fev1_fvc < df.fev1fvc_lln)),
+    ((df.fev1_pp < 50) & (df.fev1_pp >= 30) & (df.fev1_fvc < df.fev1fvc_lln)),
+    ((df.fev1_pp < 30) & (df.fev1_fvc < df.fev1fvc_lln))
+]
+
+goldstg = ("GOLD-1", "GOLD-2", "GOLD-3", "GOLD-4")
+df["GOLD_stage"] = np.select(criteria, goldstg)
+
+# ------ REMOVE segmentations with errors
+df = df[df.bp_seg_error == False]
+df = df[(df.bp_leak_score != 0) | (df.bp_segmental_score != 0) |
+        (df.bp_subsegmental_score != 0)]
+
+# ------ SAVE DF
+df.to_csv(str(outpath / "final_bp_db.csv"))
