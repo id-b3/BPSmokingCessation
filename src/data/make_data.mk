d6006a739770b20bbe73cd486568494f3e19227b
id-b3 <58172188+id-b3@users.noreply.github.com>
2023-03-10 11:46:54 +0100

    Get variable filter from file

--- src/data/make_data.mk
+++ src/data/make_data.mk
@@ -8,21 +8,26 @@ VPATH=./data/raw/
 DINT=./data/interim/
 DPRC=./data/processed/
 
+# RAW Data
 SPSS_FILES=1a_q_1.sav 1b_q_1.sav 1c_q_1.sav 1a_v_1.sav 2a_q_2.sav 2b_q_1.sav 2a_v_1.sav
-CSV_FILES=$(DINT)2b_q_1.csv $(DINT)2a_q_2.csv $(DINT)2a_v_1.csv $(DINT)1c_q_1.csv $(DINT)1b_q_1.csv $(DINT)1a_q_1.csv $(DINT)1a_v_1.csv
-BP_COLS=bp_wap,bp_la,bp_wt,bp_ir,bp_or
-
-SEG_CSV=final_bp_list.csv
 SCN_CSV=participant_age-at-scan.csv
+SEG_CSV=final_bp_list.csv
+VAR_FILTER=$(VPATH)variable_filter_list.txt
+
+# Interim Data
+CSV_FILES=$(DINT)2b_q_1.csv $(DINT)2a_q_2.csv $(DINT)2a_v_1.csv $(DINT)1c_q_1.csv $(DINT)1b_q_1.csv $(DINT)1a_q_1.csv $(DINT)1a_v_1.csv
 SPLT_CSV=$(DINT)formatted_bp_data.csv
 MRG_CSV=$(DINT)data_merged.csv
 BP_CSV=$(DINT)bronchial_parameter_db_full.csv
 BP_FILT_CSV=$(DINT)bp_db_filtered.csv
+
+# Processed Data
 BP_FINAL=$(DPRC)final_bp_db.csv
 
-DB_COLS=1,341,342,343,344,125,126,127,200,210,297,298,299,58,86,191,201,232,59,106,192,202,244,60,107,193,203,245,61,108,256,62,109,194,204,270,65,116,196,281,66,117,197,207,282,283,284,88,162,212,326,91,166,223,224,220,221,103,218,227,293,146,151,152,156,171,172,313,318,319,322,332,333,2,3,4,5,6,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57
-# DB_COLS=1,2,311,308,309,88,89,90,185,186,187,36,167,37,168,24,147,48,169,62,170,67,175,74,75,178,179,15,16,152,226,4,117,222,149,10,12,13,19,85,164,104,109,113,206,211,216,110,212,123,124,231,232,310,255,258,259,260,251,252,253,254,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,300,301,302,303,304,305,306
-
+# Lists of Variables
+BP_COLS=bp_wap,bp_la,bp_wt,bp_ir,bp_or
+# Convert the list of variables in the variable_filter_list.txt file into a string
+DB_COLS := $(shell cat $(VAR_FILTER) | tr '\n' ',' | sed 's/,$$//')
 
 all: $(BP_FINAL)
 	echo "Done"
@@ -31,6 +36,7 @@ $(BP_FINAL): $(BP_FILT_CSV)
 	< $< ./src/data/fill_missing.py
 
 $(BP_FILT_CSV): $(BP_CSV)
+	echo $(DB_COLS)
 	< $< csvcut -c $(DB_COLS) > $@
 
 # Remove any columns that contain only 1 unique value
