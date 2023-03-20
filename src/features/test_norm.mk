f3e7158572b28d53be2703036bf5b8070002e0ff
id-b3 <58172188+id-b3@users.noreply.github.com>
2023-03-20 16:50:39 +0100

    Correction to incorrect parsing of params for normality test

--- src/features/test_norm.mk
+++ src/features/test_norm.mk
@@ -4,12 +4,15 @@ SHELL := bash
 
 .PHONY: all clean
 
+comma:=,
+P_LIST := $(subst $(comma), , $(PARAMS))
 # CONDA_ACTIVATE=source $$(conda info --base)/etc/profile.d/conda.sh ; conda activate ; conda activate
 
 all: hist
 
 hist: $(BP_FINAL)
 	# $(CONDA_ACTIVATE) stats
-	for col in $(PARAMS) ; do \
+	for col in $(P_LIST); do \
+		echo $$col
 		csvcut -c "$$col,gender,GOLD_stage,copd_diagnosis,asthma_diagnosis" $< | python ./src/features/descriptive/test_normality.py ; \
 	done
